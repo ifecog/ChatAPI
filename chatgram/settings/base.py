e357c2f825a6724d13ae010707439319706e47f8
progIfe <ifeoluwasamson90@gmail.com>
2024-01-07 16:21:46 +0100

    Added and configured chat app

--- chatgram/settings/base.py
+++ chatgram/settings/base.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
